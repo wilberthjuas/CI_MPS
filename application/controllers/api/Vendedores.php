f9e0f09e00aed9719ac2700908a215ed60ec078b
baronn <baronn.r99@gmail.com>
2022-06-23 13:42:09 -0500

    Vendedores correcion mayus

--- application/controllers/api/Vendedores.php
+++ application/controllers/api/Vendedores.php
@@ -8,8 +8,8 @@ class Vendedores extends CI_Controller {
 		parent::__construct();
 		//Do your magic here
 		$this->load->helper('url');
-		$this->load->model('Vendedor_model', 'vendedores');
-		$this->load->model('util/api_utils_model', 'api_utils');
+		$this->load->model('Vendedor_Model', 'vendedores');
+		$this->load->model('util/Api_Utils_Model', 'api_utils');
 		$this->method = $this->input->server('REQUEST_METHOD');
 	}
 
